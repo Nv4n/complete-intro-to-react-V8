1adf803805df1795617dfa2a5d99ab14bbb8f5cc
Sybatron <vipsister2014@gmail.com>
2023-02-11 16:40:29 +0200

    L05-Hooks_Effects_Router

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import SearchParams from "./SearchParams";
+import Details from "./Details";
+
+const App = () => {
+	return (
+		<BrowserRouter>
+			<header>
+				<Link to="/">Adopt Me!</Link>
+			</header>
+			<Routes>
+				<Route
+					path="/details/:id"
+					element={<Details></Details>}
+				></Route>
+				<Route path="/" element={<SearchParams></SearchParams>}></Route>
+			</Routes>
+		</BrowserRouter>
+	);
+};
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
